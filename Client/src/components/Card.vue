d5f869ad8ad203ab469fe75a17898dc801ccdded
quooctrangg <nguyenquoctrang188@gmail.com>
2023-06-02 20:23:09 +0700

    add FormAdd, Card item

---
+++ Client/src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+    <div
+        class="rounded-md border-[2px] border-white bg-slate-100 bg-opacity-50 w-full p-1 focus:border-green-500 flex justify-between items-center mb-2">
+        <div class="flex w-full gap-2">
+            <div class="w-[10%]">
+                <img src="/logo.png" alt="">
+            </div>
+            <div class="flex flex-col justify-between">
+                <div class="">
+                    <h3>Test</h3>
+                </div>
+                <div class="flex gap-5 text-base">
+                    <h4 class="flex gap-1 items-center">
+                        <i class="fa-regular fa-money-bill-1"></i>
+                        <span>10.000.000đ</span>
+                    </h4>
+                    <h4 class="flex gap-1 items-center">
+                        <i class="fa-regular fa-calendar-days"></i>
+                        <span>06/02/2023</span>
+                    </h4>
+                    <h4 class="flex gap-1 items-center">
+                        <i class="fa-solid fa-message"></i>
+                        <span>Ăn hải sản,...</span>
+                    </h4>
+                </div>
+            </div>
+        </div>
+        <div class="p-5 cursor-pointer text-red-600 hover:text-red-400">
+            <i class="fa-solid fa-trash"></i>
+        </div>
+    </div>
+</template>
---
+++ Client/src/components/FormAdd.vue
@@ -0,0 +1,31 @@
+<template>
+    <form class="w-full  flex flex-col gap-3">
+        <input type="text"
+            class="rounded-md border-[3px] border-white bg-slate-100 h-[100%] bg-opacity-50 w-full p-2 focus:border-green-500 outline-0 text-base"
+            placeholder="Tiêu đề">
+        <input type="number"
+            class="rounded-md border-[3px] border-white bg-slate-100 h-[100%] bg-opacity-50 w-full p-2 focus:border-green-500 outline-0 text-base"
+            placeholder="Số tiền">
+        <input type="date"
+            class="rounded-md border-[3px] border-white bg-slate-100 h-[100%] bg-opacity-50 w-full p-2 focus:border-green-500 outline-0 text-base text-gray-400">
+        <select name="" id=""
+            class="rounded-md border-[3px] border-white bg-slate-100 h-[100%] bg-opacity-50 w-full p-2 focus:border-green-500 outline-0 text-base text-gray-400">
+            <option value="">Chọn</option>
+            <option value="">Giải trí</option>
+            <option value="">Sức khỏe</option>
+            <option value="">Ăn uống</option>
+            <option value="">Di chuyển</option>
+            <option value="">Phiếu lương</option>
+            <option value="">Khác</option>
+        </select>
+        <textarea name="" id="" rows="5" placeholder="Ghi chú"
+            class="rounded-md border-[3px] border-white bg-slate-100 h-[100%] bg-opacity-50 w-full p-2 focus:border-green-500 outline-0 text-base"></textarea>
+        <div class="w-full flex justify-center">
+            <button type="submit"
+                class="w-auto border py-1 px-2 rounded-lg bg-green-500 hover:bg-green-300 flex items-center gap-2 text-gray-700">
+                <i class="fa-solid fa-plus"></i>
+                Thêm
+            </button>
+        </div>
+    </form>
+</template>
