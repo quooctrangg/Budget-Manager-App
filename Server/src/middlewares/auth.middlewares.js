c0a1271b42214bb1b630461bf5fc0b2caf71843b
quooctrangg <nguyenquoctrang188@gmail.com>
2023-06-19 20:23:07 +0700

    fix api chart

--- Server/src/middlewares/auth.middlewares.js
+++ Server/src/middlewares/auth.middlewares.js
@@ -9,7 +9,10 @@ const checkLogin = async (req, res, next) => {
         if (!token) return res.json(new ApiRes(401, 'failed', 'Bạn chưa đăng nhập!', null))
         let user = jwt.verify(token, process.env.ACCESS_TOKEN)
         let data = await userDB.findOne({ _id: user.id })
-        if (data) next()
+        if (data) {
+            req.id = data._id
+            next()
+        }
         else return res.json(new ApiRes(400, 'failed', 'Không tìm thấy người dùng!', null))
     } catch (error) {
         return res.json(new ApiRes(401, 'failed', 'Bạn chưa đăng nhập!', null))
